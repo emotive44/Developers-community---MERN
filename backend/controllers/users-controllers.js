3ec1dd287f1541e1560fabba4f95c7c5ebe28f2f
marko streleshki <marko_streleshki_96@abv.bg>
2020-05-01 22:12:40 +0300

    sign up with hashed password

--- backend/controllers/users-controllers.js
+++ backend/controllers/users-controllers.js
@@ -1,14 +1,59 @@
 const { validationResult } = require('express-validator');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+const User = require('../models/user');
 
 
-const signup = (req, res, next) => {
+const signup = async (req, res) => {
   const errors = validationResult(req);
   
   if(!errors.isEmpty()) {
     res.status(400).json({ errors: errors.array() });
   }
 
-  res.status(201).json({msg: 'Sign up'})
+  const { name, email, password } = req.body;
+  const avatar = gravatar.url(email, {
+    s: '200',
+    r: 'pg',
+    d: 'mm'
+  });
+  
+  let existUser;
+  try {
+    existUser = await User.findOne({email});
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ errors: [{ msg: 'Signing up failed, please try again.' }] });
+  }
+
+  if(existUser) {
+    res.status(422).json({ errors: [{ msg: 'User already exist, try with new data' }] });
+  }
+
+  let hashPassword;
+  try {
+    hashPassword = await bcrypt.hash(password, 12);
+  } catch(err) {
+    console.error(err);
+    res.status(500).json({ errors: [{ msg: 'Could not create a user, please try again.' }] });
+  }
+
+  const newUser = new User({
+    name,
+    email,
+    avatar,
+    password: hashPassword
+  })
+
+  try {
+    await newUser.save();
+  } catch(err) {
+    console.err(err);
+    res.status(500).json({ errors: [{ msg: 'Signing up failed, please try again.' }] });
+  }
+
+  res.status(201).json({userId: newUser.id})
 }
 
 module.exports = {
