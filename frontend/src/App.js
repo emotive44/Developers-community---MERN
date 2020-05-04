d9713968f6d301cf35021b5a88e5ed6a508635c7
marko streleshki <marko_streleshki_96@abv.bg>
2020-05-05 01:56:18 +0300

    setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Landing from './components/common/Landing';
 import NavBar from './components/common/NavBar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+
 
 const App = () => {
   return (
     <Fragment>
-      <NavBar />
-      <Landing />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path='/' component={Landing}/>
+          <Route path='/login' component={Login}/>
+          <Route path='/register' component={Register}/>
+        </Switch>
+      </Router>
     </Fragment>
   );
 }
