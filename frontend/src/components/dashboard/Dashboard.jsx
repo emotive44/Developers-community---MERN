621961d534030609fd9c175f5a245a9f6e99a144
marko streleshki <marko_streleshki_96@abv.bg>
2020-05-09 00:19:20 +0300

    get current profile and attach to store

--- frontend/src/components/dashboard/Dashboard.jsx
+++ frontend/src/components/dashboard/Dashboard.jsx
@@ -1,6 +1,12 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-const Dashboard = () => {
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
+
+const Dashboard = ({ auth, profile, getCurrentProfile }) => {
+  useEffect(() => {
+    if(auth.isAuth) getCurrentProfile();
+  }, []);
   return (
     <div>
       Dashboard
@@ -8,4 +14,9 @@ const Dashboard = () => {
   );
 }
 
-export default Dashboard;
+const mapStateToProps = state => ({
+  profile: state.profile,
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
